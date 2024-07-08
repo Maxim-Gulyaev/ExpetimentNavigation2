5f313d263afca45020d0af0125e01379634a6f5d
Guliaev Maxim <maxim_@bk.ru>
2024-07-08 16:12:23 +0300

    Initial commit

---
+++ feature/src/main/java/com/maxim/feature/MyClass.kt
@@ -0,0 +1,4 @@
+package com.maxim.feature
+
+class MyClass {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ExpetimentNavigation2"
+include(":app")
+include(":featureA")
+include(":featureB")
+include(":featureC")
+include(":core")
